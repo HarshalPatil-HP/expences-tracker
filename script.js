12c9977b64d05d99cc220a41f3d45cdbd84bbf26
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-07-27 21:39:24 +0530

    Fix formatting issues in renderExpenses function

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
 function renderExpenses() {
   list.innerHTML = "";
   let total = 0;
-
   expenses.forEach((exp, index) => {
     total += exp.amount;
 
